0fff91fc83ff90006d856c4f8ad647064d21e7dd
binbin <binbin@binbin-PC>
2016-09-24 22:36:13 +0800

    add product msg and change some css

--- WebContent/js/self/main.js
+++ WebContent/js/self/main.js
@@ -2,31 +2,10 @@
  * 
  */
 $(function() {
-	 $('#slider').nivoSlider({pauseTime: 5000});
-	 var speed=30//速度数值越大速度越慢
-	var colee_left2=document.getElementById("colee_left2");
-	var colee_left1=document.getElementById("colee_left1");
-	var colee_left=document.getElementById("colee_left");
-	colee_left2.innerHTML=colee_left1.innerHTML;
-	function Marquee3(){
-	if(colee_left2.offsetWidth-colee_left.scrollLeft<=0)//offsetWidth 是对象的可见宽度
-	colee_left.scrollLeft-=colee_left1.offsetWidth;//scrollWidth 是对象的实际内容的宽，不包边线宽度
-	else{
-	colee_left.scrollLeft++;
-	}
-	}
-	var MyMar3=setInterval(Marquee3,speed)
-	colee_left.onmouseover=function() {clearInterval(MyMar3)}
-	colee_left.onmouseout=function() {MyMar3=setInterval(Marquee3,speed)}
-	//iFrameHeight();
+	 $('#slider').nivoSlider({pauseTime: 5000,controlNav:false});
+	iFrameHeight();
 });
 
-function change_page(id){
-	$("#iframepage").attr("src",id);
-}
-function back_page(id){
-	$("#iframepage", parent.document).attr("src",id);
-}
 function js_menu(id){
 	console.debug(id);
 	var js_index=0;
@@ -52,3 +31,20 @@ function iFrameHeight() {
 	   ifm.width = subWeb.body.scrollWidth;
 	}   
 }
+function change_page(id){
+	$("#iframepage").attr("src",id);
+	document.getElementById('body').scrollIntoView();
+}
+function back_page(id){
+	$("#iframepage", parent.document).attr("src",id);
+}
+function returnFalse(){
+	return false;s
+}
+$(document).bind("contextmenu",function(e){
+    if (!jQuery_isTagName(e, ['div', 'TEXTAREA'])) {
+        e.preventDefault();
+        return false;
+    }
+    return true;
+});
